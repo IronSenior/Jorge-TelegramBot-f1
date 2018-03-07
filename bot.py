e70ebc736721e5c03ad9601943d04c2c782392f3
Jose Marquez <pepemarquezof@gmail.com>
2018-03-07 19:02:12 +0100

    Primer commit

---
+++ bot.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import funciones as func
+import token as tuko
+import time
+import random
+import csv
+
+token = tuko.token()
+bot = telegram.Bot(token=token)
+updater = Updater(token=token)
+
+dispatcher = updater.dispatcher
+
+
+def send(message, bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text=message)
+
+def competition(bot, update, args):
+	if args == []:
+		send("Error!! Tienes que decirme el nombre de la competición y la password", bot, update)
+
+	elif args[0] and args[1]:
+		print "hola"
+		nombre = str(args[0])
+		passwd = str(args[1])
+		if func.existe_comp(nombre):
+			send("Ese nombre no es válido o ya está en uso", bot, update)
+		else:
+			competition = [nombre, passwd]
+			func.crear_comp(competition)
+			send("Has comenzado la competición con éxito", bot, update)
+			send("Ahora todos se pueden unir", bot, update)
+
+def unir(bot, update, args):
+	comp = args[0]
+	password = args[1]
+	id_player = args[2]
+	#permiso = func.passwd_comp(comp, password, id_player) 
+	
+	if True:
+		func.add_player(comp, id_player)
+		send("Jugador añadido", bot, update)
+
+	else:
+		send("Error! Ha introducido mal los datos de la competición")
+
+
+
+
+competition_handler = CommandHandler('start_competition', competition, pass_args=True)
+matricula_handler = CommandHandler('unirme', unir, pass_args=True)
+
+dispatcher.add_handler(competition_handler)
+dispatcher.add_handler(matricula_handler)
+
+updater.start_polling()
---
+++ funciones.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import token as tuko
+import time
+import random
+import csv
+
+token = tuko.token()
+bot = telegram.Bot(token=token)
+updater = Updater(token=token)
+
+dispatcher = updater.dispatcher
+
+def send(message, bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text=message)
+
+def existe_comp(nombre):
+	existe = False
+	print "estoy en funciones"
+	with open("./competitions/competitions.csv", 'rb') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=",")
+		for row in reader:
+			name_c = row['name']
+			if nombre == str(name_c):
+				existe = True
+	return existe
+
+
+def crear_comp(competition):
+	name = competition[0]
+	inicio = ['player_id','c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','c11','c12','c13','c14','c15','c16','c17','c18','c19','Tpoints']
+	with open('./competitions/competitions.csv', 'a') as f:
+		writer = csv.writer(f, delimiter=",")
+		writer.writerow(competition)
+
+	with open('./competitions/%s.csv'%(name), 'a') as f:
+		writer = csv.writer(f, delimiter=",")
+		writer.writerow(inicio)
+
+	
+def add_player(comp, player):
+	row = [player,'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']
+
+	with open('./competitions/%s.csv'%(comp), 'a') as f:
+		writer = csv.writer(f, delimiter=",")
+		writer.writerow(row)
